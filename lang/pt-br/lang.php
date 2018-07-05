811fca58fc2e48c03c7e234fc73b64d92a3a0c07
Alexandre Belchior <alexbelchior@gmail.com>
2018-07-05 13:35:05 +0200

    translation update

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['title']                 = 'Projeto Redmine';
+$lang['desctitle']             = 'Descrição';
+$lang['vertitle']              = 'Versões';
+$lang['private']               = 'Projeto privado / Desconhecido';
+$lang['info']                  = 'Informação';
+$lang['norights']              = 'Você não participa deste projeto ou não tem os privilégios para visualizar esta informação';
+$lang['mainproj']              = 'Projeto principal';
+$lang['subproject']            = 'Subprojeto de:';
+$lang['description']           = 'Nenhuma descrição está disponível para este projeto.';
+$lang['createdon']             = 'Criado em:';
+$lang['updatedon']             = 'Atualizado em:';
+$lang['noversion']             = 'Nenhuma versão está disponível para este projeto.';
+$lang['hdetail']               = 'Detalhes do projeto';
+$lang['tversion']              = 'Há atualmente';
+$lang['vversion']              = 'versões,';
+$lang['issues']                = 'problemas incluídos';
+$lang['open']                  = 'aberto e';
+$lang['membdetail']            = 'membros envolvidos no projeto.';
+$lang['membres']               = 'Membros';
